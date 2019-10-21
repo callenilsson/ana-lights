4770c91df521fa62b5e76bffe02a96fb41a10c49
irdeen <calle_71@hotmail.com>
2019-10-21 15:39:31 +0200

    .

--- laptop_client_movie.py
+++ laptop_client_movie.py
@@ -8,14 +8,8 @@ def text_time_to_seconds(text_time):
     milliseconds = int(text_time[text_time.index(':')+1 :])
     return minutes*60 + seconds + milliseconds/100.0
 
-if __name__ == "__main__":
-    rpi1 = socket.socket()
-    rpi2 = socket.socket()
-    rpi3 = socket.socket()
-    rpi1.connect(('192.168.0.150', 9090))
-    rpi2.connect(('192.168.0.179', 9090))
-    rpi3.connect(('192.168.0.197', 9090))
-
+def start(pies):
+    for pi in pies: pi.send('start'.encode())
     songs = [['Cloudless skies', '0:03:69'],
             ['Pixeldye', '4:19:52'],
             ['Lumiére', '12:22:87'],
@@ -24,26 +18,48 @@ if __name__ == "__main__":
             ["You're somewhere", '27:23:13']]
     for i in range(len(songs)):
         print(i+1, '-', songs[i][0], '(' + songs[i][1] + ')')
-    text_input = input('Enter song or custom timecode to start from:')
+    text_input = input('Enter song or custom timecode to start from: ')
     if text_input.isdigit():
         song = songs[int(text_input)-1]
         song_start = text_time_to_seconds(song[1])
     else:
         song_start = text_time_to_seconds(text_input)
-
     # Send timecode to RPi's where to start
-    rpi1.send(str(song_start).encode())
-    rpi2.send(str(song_start).encode())
-    rpi3.send(str(song_start).encode())
-
+    for pi in pies: pi.send(str(song_start).encode())
     # Wait for ready responses from RPi's
-    print(rpi1.recv(1024).decode())
-    print(rpi2.recv(1024).decode())
-    print(rpi3.recv(1024).decode())
-
+    for pi in pies: pi.recv(1024).decode()
     # Ready
     input('Press enter to start')
-    start_time = time.time()
-    rpi1.send(str(start_time).encode())
-    rpi2.send(str(start_time).encode())
-    rpi3.send(str(start_time).encode())
+    for pi in pies: pi.send('go'.encode())
+
+def stop(pies):
+    for pi in pies: pi.send('stop'.encode())
+
+def pause(pies):
+    for pi in pies: pi.send('pause'.encode())
+
+def exitt(pies):
+    for pi in pies: pi.close()
+    exit()
+
+if __name__ == "__main__":
+    rpi1 = socket.socket()
+    rpi2 = socket.socket()
+    rpi3 = socket.socket()
+    rpi1.connect(('192.168.0.150', 9090))
+    rpi2.connect(('192.168.0.179', 9090))
+    rpi3.connect(('192.168.0.197', 9090))
+    pies = [rpi1, rpi2, rpi3]
+
+    while True:
+        print('---------------')
+        print('1 - Start')
+        print('2 - Stop')
+        print('3 - Pause')
+        print('4 - Exit')
+        action = input('Select an action to perform:')
+
+        if action == '1': start(pies)
+        if action == '2': stop(pies)
+        if action == '3': pause(pies)
+        if action == '4': exitt(pies)
