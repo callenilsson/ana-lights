4989089c703df8c3d504e32e365632d96011f4fc
calle <calle_71@hotmail.com>
2019-10-30 23:35:25 +0100

    .

--- zero_server_movie.py
+++ zero_server_movie.py
@@ -91,7 +91,7 @@ if __name__ == '__main__':
     conn, client_address = server.accept()
 
     laptop_time = float(conn.recv(1024).decode())
-    time.sleep(5)
+    time.sleep(10)
     pi_time = time.time()
     diff_time = laptop_time - pi_time
     print(diff_time)
