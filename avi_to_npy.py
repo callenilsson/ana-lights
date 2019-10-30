a8807230b4431755cf23e11291e933bacf4ca61a
calle <calle_71@hotmail.com>
2019-10-30 21:20:46 +0100

    .

--- avi_to_npy.py
+++ avi_to_npy.py
@@ -1,8 +1,8 @@
 import skvideo.io
 import numpy as np
 
-video = skvideo.io.vread('videos/ana_lights_gbg6.avi')[:, :288, :1, :]
+video = skvideo.io.vread('videos/ana_lights_gbg6.avi')[:10000, :288, :1, :]
 video = video.astype(np.uint8)
 print(video.shape)
-np.save('lights/ana_lights_gbg.npy', video)
+np.save('lights/ana_lights_gbg_lite.npy', video)
     
