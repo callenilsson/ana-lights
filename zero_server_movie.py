81b60e4676b6245cfa539c771ff103b7e4e285ae
calle <calle_71@hotmail.com>
2020-02-29 15:49:16 +0100

    .

--- zero_server_movie.py
+++ zero_server_movie.py
@@ -92,7 +92,7 @@ if __name__ == '__main__':
     strip.begin()
 
     print('Loading video...')
-    video = np.load('lights/ana_lights_gbg.npy')*0.2
+    video = np.load('lights/ana_lights_gbg.npy')
     video_ending = np.load('lights/ana_ending.npy')
     fps = 30
 
