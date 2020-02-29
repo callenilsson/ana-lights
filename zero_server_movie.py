7f63730d40fa06589f673695fe2fc1265b8f1292
calle <calle_71@hotmail.com>
2020-02-29 16:01:40 +0100

    .

--- zero_server_movie.py
+++ zero_server_movie.py
@@ -93,7 +93,7 @@ if __name__ == '__main__':
     print('Loading video...')
     video = np.load('lights/ana_lights_gbg.npy')
     video_ending = np.load('lights/ana_ending.npy')
-    fps = 30
+    fps = 60
 
     server = socket.socket()
     server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
