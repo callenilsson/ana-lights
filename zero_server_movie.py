48f0a9ca0a1718ec79a4493f859ae65342d44ada
calle <calle_71@hotmail.com>
2020-06-19 09:32:51 +0200

    .

--- zero_server_movie.py
+++ zero_server_movie.py
@@ -143,7 +143,7 @@ if __name__ == '__main__':
         time.sleep(1)
         server = socket.socket()
         server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        server.bind(('0.0.0.0', 9092))
+        server.bind(('0.0.0.0', 9091))
         server.listen(1)
         stripStatus(strip, [10,0,0])
         print('Ready')
@@ -208,7 +208,7 @@ if __name__ == '__main__':
 
             elif action_recv == 'stream':
                 if action == 'stop' or action == 'pause' or action == 'ready':
-                        action = 'stream'
-                        barrier.wait()
-                    else:
-                        action = 'stream'
+                    action = 'stream'
+                    barrier.wait()
+                else:
+                    action = 'stream'
