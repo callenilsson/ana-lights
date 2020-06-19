f6e06642189b7c5a48b92ad2081b18bc1715fbdd
calle <calle_71@hotmail.com>
2020-06-19 12:22:05 +0200

    .

--- zero_laptop_client_movie.py
+++ zero_laptop_client_movie.py
@@ -126,8 +126,8 @@ def stream_thread(stream_pies):
 if __name__ == "__main__":
     print('Scanning for pies...')
     found_pies = get_pies_on_network()
-    pies = connect_pies(found_pies, 9092)
-    stream_pies = connect_pies(found_pies, 9095)
+    pies = connect_pies(found_pies, 9093)
+    stream_pies = connect_pies(found_pies, 9096)
     threading.Thread(target=stream_thread, args=(stream_pies,)).start()
 
     while True:
